3be25b40eb636c3de274c08c33f5d2175dc5fdbe
mUniKeS <munikes@members.fsf.org>
2014-06-02 14:46:28 +0200

    fix the unneccesary imports

--- quiniela/core/views.py
+++ quiniela/core/views.py
@@ -34,9 +34,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.db.models import Sum
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-import random
-import datetime
-import time
 
 @login_required
 def principal(request, template_name='core/main.html', jornada=None):
