6acf7ccf215973e1a1775ed69ad923661999bf39
mUniKeS <munikes@members.fsf.org>
2016-09-02 10:34:00 +0200

    add bootstrap to project and do the login and logout

--- quiniela/settings.py
+++ quiniela/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'quiniela_2015',                      # Or path to database file if using sqlite3.
+        'NAME': 'quiniela',                      # Or path to database file if using sqlite3.
         'USER': 'quiniela',                      # Not used with sqlite3.
         'PASSWORD': 'quiniela',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'django_nvd3',
     'djangobower',
+    'bootstrap3',
     'quiniela.core',
 )
 
